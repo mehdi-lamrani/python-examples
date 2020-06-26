5f4dec9c4328d79c43951365c3dee038811291a2
MehdiLamrani <mehdi.lamrani@gmail.com>
2020-06-26 13:20:09 +0200

    Initial Commit

--- Sequence.py
+++ Sequence.py
@@ -1,19 +1,22 @@
 from functools import reduce
 
-
+#### simple MAP example
 my_add = lambda a, b :a + b    
-list(map(my_add, [1, 2, 3], [4, 5, 6]))
+res = list(map(my_add, [1, 2, 3], [4, 5, 6]))
+print(res)
 
-
+#### simple FILTER example
 def my_check(a):  
-    return a > 10 and a%2  
-list(filter(my_check, [1, 5, 10, 11, 12, 25])) 
+    return a > 10 and a%2
+res = list(filter(my_check, [1, 5, 10, 11, 12, 25]))
+print(res)
 
- list(zip([1, 2, 3], [4, 5, 6])) 
+#### simple ZIP example
+res = list(zip([1, 2, 3], [4, 5, 6]))
+print(res)
 
+#### simple REDUCE example
 res = reduce(lambda a, b : a if a>b else b,
 	    [3, 4, 6, 9, 34, 12])
-
-
 print(res)
 
